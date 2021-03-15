e8eaaafbd1a1ec139e7774cf6f8feb84e15bd46a
hans0 <kiessler.hans@gmail.com>
2021-03-15 17:46:18 -0400

    Done with MVP, messed up pull request

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -41,69 +41,45 @@ const memoryResult = (state, operation, target) => {
         return state.total;
     }
   }
-  
-  // console.log(state, operation, target)
-  // console.log(target === "memory")
-  // if (target === "memory"){
-  //   console.log("memory")
-  //   switch(operation) {
-  //     case("M+"):
-  //       console.log("M+");
-  //       return state.total;
-  //     case("MC"):
-  //       return 0;
-  //     case("MR"):
-  //       return 0;
-  //   }
-  // } else if (target === "total") {
-  //   switch(operation) {
-  //     case("M+"):
-  //       return 0;
-  //     case("MC"):
-  //       return 0;
-  //     case("MR"):
-  //       return state.memory;
-  //   }
-  // }
   return failNumber;
 }
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case(ADD_ONE):
-            return({
-                ...state,
-                total: state.total + 1
-            });
+  switch(action.type) {
+    case(ADD_ONE):
+      return({
+        ...state,
+        total: state.total + 1
+      });
 
-        case(APPLY_NUMBER):
-            return ({ 
-                ...state, 
-                total: calculateResult(state.total, action.payload, state.operation)
-            });
-        
-        case(CHANGE_OPERATION):
-            return ({
-                ...state,
-                operation: action.payload
-            });
-            
-        case(MEMORY_OPERATION):
-            return({
-                ...state,
-                memory: memoryResult(state, action.payload, "memory"),
-                total: memoryResult(state, action.payload, "total")
-            })
+    case(APPLY_NUMBER):
+      return ({ 
+        ...state, 
+        total: calculateResult(state.total, action.payload, state.operation)
+      });
+      
+      case(CHANGE_OPERATION):
+        return ({
+          ...state,
+          operation: action.payload
+        });
+          
+      case(MEMORY_OPERATION):
+        return({
+          ...state,
+          memory: memoryResult(state, action.payload, "memory"),
+          total: memoryResult(state, action.payload, "total")
+        })
 
-        case(CLEAR_DISPLAY):
-            return({
-                ...state, 
-                total: 0
-            });
+      case(CLEAR_DISPLAY):
+        return({
+          ...state, 
+          total: 0
+        });
 
-        default:
-            return state;
-    }
+      default:
+        return state;
+  }
 }
 
 export default reducer;
